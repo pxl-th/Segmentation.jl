a658862d0ff34e2377141462142cb408a98ede7f
Anton Smirnov <anton.smn@protonmail.com>
2021-05-09 01:08:26 +0300

    Implement UNetDecoder

--- src/Segmentation.jl
+++ src/Segmentation.jl
@@ -1,5 +1,41 @@
 module Segmentation
 
-greet() = print("Hello World!")
+using Flux
 
-end # module
+include("decoder.jl")
+
+"""
+Segmentation inference:
+    1. [features] = encoder(x)
+    2. decoder_output = decoder([features]...)
+    3. masks = segmentation_head(decoder_output)
+    [4]. labels = classification_head([features][-1])
+"""
+
+"""
+E - encoder
+D - decoder
+S - segmentation
+C - classification
+"""
+# abstract type Seg{E, D, S, C} end
+# struct UNet{E, D, S, C} <: Seg{E, D, S, C} end
+
+d = DecoderBlock(4, 8, 12)
+
+x = randn(Float32, 10, 10, 12, 1)
+o = d(x)
+@info size(o)
+
+x = randn(Float32, 10, 10, 4, 1)
+s = randn(Float32, 20, 20, 8, 1)
+o = d(x, s)
+@info size(o)
+
+
+decoder = UNetDecoder(
+    [3, 32, 24, 40, 112, 320],
+    [256, 128, 64, 32, 16],
+)
+
+end
---
+++ src/decoder.jl
@@ -0,0 +1,77 @@
+struct DecoderBlock{C, D}
+    # TODO attention1
+    # TODO attention2
+    conv1::C
+    conv2::D
+end
+Flux.@functor DecoderBlock
+
+function DecoderBlock(in_channels, skip_channels, out_channels)
+    conv1 = conv2drelu(
+        (3, 3), (in_channels + skip_channels)=>out_channels; pad=1,
+    )
+    conv2 = conv2drelu((3, 3), out_channels=>out_channels; pad=1)
+    DecoderBlock(conv1, conv2)
+end
+
+function (d::DecoderBlock)(
+    x::AbstractArray{T}, skip::Union{AbstractArray{T}, Nothing} = nothing
+) where T
+    x = upsample_nearest(x, (2, 2))
+    if skip ≢ nothing
+        x = cat(x, skip; dims=3) # TODO |> attention1
+    end
+    x |> d.conv1 |> d.conv2 # TODO |> attention2
+end
+
+function conv2drelu(
+    kernel::Tuple{Int64, Int64}, channels::Pair{Int64, Int64};
+    pad::Int64 = 0, stride::Int64 = 1,
+)
+    Chain(
+        Conv(kernel, channels; stride, pad, bias=false),
+        BatchNorm(channels[2]), # bias=false because of batch norm.
+        x -> x .|> relu,
+    )
+end
+
+struct UNetDecoder{B}
+    blocks::B
+end
+Flux.@functor UNetDecoder
+
+function UNetDecoder(
+    encoder_channels::AbstractVector{Int64},
+    decoder_channels::AbstractVector{Int64};
+    n_blocks::Int64 = 5
+)
+    if length(decoder_channels) != n_blocks
+        throw(
+            "Number of decoder channels is not the same as the " *
+            "number of blocks. ($(length(decoder_channels)) vs $n_blocks)"
+        )
+    end
+
+    encoder_channels = encoder_channels[end:-1:2]
+    head_channels = encoder_channels[1]
+    in_channels = [head_channels; decoder_channels[1:end - 1]]
+    skip_channels = [encoder_channels[2:end]; 0]
+
+    UNetDecoder([
+        DecoderBlock(inc, sc, oc)
+        for (inc, sc, oc)
+        in zip(encoder_channels, skip_channels, decoder_channels)
+    ])
+end
+
+function (d::UNetDecoder)(features)
+    features = features[end:-1:2]
+    head = features[1]
+    skips = features[2:end]
+
+    x = head
+    for (i, block) in d.blocks
+        x = block(x, i ≤ length(skips) ? skips[i] : nothing)
+    end
+    x
+end
