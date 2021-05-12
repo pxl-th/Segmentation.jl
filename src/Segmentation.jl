9c5323f47cef924c13103614a647656c8a2d40cf
Anton Smirnov <anton.smn@protonmail.com>
2021-05-12 22:56:06 +0300

    Add comma10k training

--- src/Segmentation.jl
+++ src/Segmentation.jl
@@ -5,6 +5,9 @@ using ResNet
 using Images
 using DataLoaders
 using Interpolations
+using Random
+using ProgressMeter
+using ParameterSchedulers: Scheduler, Cos
 
 include("decoder.jl")
 include("head.jl")
@@ -41,6 +44,12 @@ struct Dataset
     palette::Vector{Gray}
 end
 
+function get_pb(n, desc::String)
+    Progress(
+        n; desc, dt=1, barglyphs=BarGlyphs("[=> ]"), barlen=50, color=:white,
+    )
+end
+
 DataLoaders.nobs(d::Dataset) = d.files |> length
 function DataLoaders.getobs(d::Dataset, i::Int64)
     name = d.files[i]
@@ -60,7 +69,7 @@ end
 function load_file_names(path::String)
     train = String[]
     valid = String[]
-    for f in path |> readlines
+    for f in readlines(path)[1:100]
         push!(endswith(f, "9.png") ? valid : train, split(f, "masks/")[end])
     end
     train, valid
@@ -75,7 +84,9 @@ function get_palette()
         [204, 0, 255],   # my car
         [0, 0, 0]        # for padding
     ]
-    [Gray{N0f8}(RGB{N0f8}((p ./ 255)...)) for p in palette]
+    color_palette = [RGB{N0f8}((p ./ 255)...) for p in palette]
+    bw_palette = color_palette .|> Gray{N0f8}
+    color_palette, bw_palette
 end
 
 function mask_to_classes(mask, palette)
@@ -87,45 +98,109 @@ function mask_to_classes(mask, palette)
     permutedims(classes, (2, 3, 1))
 end
 
+"""
+classes: (W, H, C)
+"""
+function probs_to_mask(classes, palette::Vector{T}) where T
+    width, height = size(classes)[1:2]
+    mask = Array{T}(undef, width, height)
+
+    for i in 1:width, j in 1:height
+        @inbounds mask[i, j] = palette[argmax(classes[i, j, :])]
+    end
+    mask
+end
+
 function main()
-    epochs = 1
-    batch_size = 1
-    palette = get_palette()
-    classes = palette |> length
-    resolution = (18 * 32, 14 * 32)
+    # TODO reduce memory usage
+    # TODO improve performance
+    # TODO accumulate gradient
+    # TODO image augmentation
+    # TODO colorize mask output
+
+    device = gpu
+    epochs = 100
+    batch_size = 2
+    color_palette, bw_palette = get_palette()
+    classes = bw_palette |> length
+    # resolution = (18 * 32, 14 * 32)
+    # resolution = (13 * 32, 10 * 32)
+    resolution = (10 * 32, 7 * 32)
 
     base_dir = raw"C:\Users\tonys\projects\comma10k"
     train_files, valid_files = load_file_names(
         joinpath(base_dir, "files_trainable"),
     )
 
-    train_dataset = Dataset(base_dir, train_files, resolution, palette)
-    train_loader = DataLoader(train_dataset, batch_size)
-    valid_dataset = Dataset(base_dir, valid_files, resolution, palette)
+    valid_dataset = Dataset(base_dir, valid_files, resolution, bw_palette)
     valid_loader = DataLoader(valid_dataset, batch_size)
 
-    optimizer = ADAM()
+    λ1 = 1e-4
+    λ0 = λ1 / 50
+    period = epochs * length(train_files) ÷ batch_size
+    optimizer = Scheduler(Cos(;λ0, λ1, period), ADAM())
+
     model = UNet(;
         classes, encoder=ResNetModel(;size=18, in_channels=3, classes=nothing),
-    )
+    ) |> device
     trainables = model |> params
 
+    @info "Image resolution: $resolution [width, height]"
+    @info "Train images: $(length(train_files))"
+    @info "Validation images: $(length(valid_files))"
+    @info "Total Parameters: $(length(trainables))"
+    @info "LR Scheduler: [λ0=$λ0, λ1=$λ1], period=$period"
+
+    @info "--- Training ---"
     for epoch in 1:epochs
+        train_loader = DataLoader(
+            Dataset(base_dir, shuffle!(train_files), resolution, bw_palette),
+            batch_size,
+        )
+
         model |> trainmode!
+        train_loss = 0.0
+        bar = get_pb(length(train_loader), "[epoch $epoch | training]: ")
         for (x, y) in train_loader
+            x, y = x |> device, y |> device
             grads = gradient(trainables) do
-                Flux.logitcrossentropy(x |> model, y; dims=3)
+                loss = Flux.logitcrossentropy(x |> model, y; dims=3)
+                train_loss += loss
+                loss
             end
             Flux.Optimise.update!(optimizer, trainables, grads)
-            break
+
+            GC.gc()
+            bar |> next!
         end
+        train_loss /= length(train_loader)
+        @info "epoch $epoch | train loss $train_loss | lr $(optimizer.optim.eta)"
 
         model |> testmode!
-        for (x, y) in valid_loader
-            l = Flux.logitcrossentropy(x |> model, y; dims=3)
-            println(l)
-            break
+        bar = get_pb(length(valid_loader), "[epoch $epoch | testing]: ")
+        validation_loss = 0.0
+        for (i, (x, y)) in enumerate(valid_loader)
+            x, y = x |> device, y |> device
+            o = x |> model
+            loss = Flux.logitcrossentropy(o, y; dims=3)
+            validation_loss += loss
+
+            if i == 1
+                o = softmax(o; dims=3) |> cpu
+                pred = probs_to_mask(o[:, :, :, 1], color_palette)
+                save("./pred-$epoch.png", permutedims(pred, (2, 1)))
+
+                y = y |> cpu
+                pred = probs_to_mask(y[:, :, :, 1], color_palette)
+                save("./mask-$epoch.png", permutedims(pred, (2, 1)))
+            end
+
+            GC.gc()
+            bar |> next!
         end
+
+        validation_loss /= length(valid_loader)
+        @info "epoch $epoch | validation loss $validation_loss"
     end
 end
 main()
